a8976d64fab3438377adbe093c1be5c75747bd29
Govind Sharma <govindsankhi24@gmail.com>
2026-07-07 11:24:57 +0530

    Change default server port to 4000
    Port 3000 is used by Leela infra on the host.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require("http");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const hostname = process.env.HOSTNAME || "0.0.0.0";
 
 const app = next({ dev, hostname, port });
